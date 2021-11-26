146cf30681f9b9e426457000792c65dd0dd18369
FranciscoMorenoSantaella <santaellamorenofrancisco@gmail.com>
2021-11-26 14:16:29 +0100

    Inicio del proyecto

---
+++ src/main/java/controller/SecondaryController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module controller {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens controller to javafx.fxml;
+    exports controller;
+}
