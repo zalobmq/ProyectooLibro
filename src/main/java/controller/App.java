4a755330946533a34cf6fbeeed79b922d5037074
zalobmq <gbretonesmoraquero@gmail.com>
2021-11-30 10:05:32 +0100

    Cambios en el app

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * JavaFX App
@@ -24,6 +27,21 @@ public class App extends Application {
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
+    }
+    public static void loadScene(Stage stage, String fxml, String title) throws IOException {
+        Parent p = loadFXML(fxml);
+        Scene s = new Scene(p);
+        stage.setScene(s);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle(title);
+      //  stage.getIcons().add(new Image(Objects.requireNonNull(App.class.getResourceAsStream("iconoDisco.png"))));
+      //  stage.getScene().getStylesheets().add(String.valueOf(App.class.getResource("estilos.css")));
+
+        stage.showAndWait();
+    }
+    
+    public static void closeScene(Stage stage) {
+        stage.close();
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
