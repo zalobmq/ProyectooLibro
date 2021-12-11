081916607601ea8a1eb6c31529266cf119e4b5dd
Jkinjt <l02jitoj@gmail.com>
2021-12-11 18:42:21 +0100

    inicio

--- src/main/java/model/h2DAO/AuthorImpH2.java
+++ src/main/java/model/h2DAO/AuthorImpH2.java
@@ -14,28 +14,46 @@ import utils.PersistenceUnit;
 
 public class AuthorImpH2{
 	
+	
+	
+	/*
+	 * Método que devuelve emf
+	 * @Return emf.createEntityManager();
+	 * */
 	public static EntityManager createEM() {
 		EntityManagerFactory emf=PersistenceUnit.getInstance("aplicacionH2");
 		
 		return emf.createEntityManager();
 	}
+	/*
+	 * Método que crea el objeto para realizar las transacciones
+	 * @Return em.getTransaction() 
+	 * */
 	
 	public static EntityTransaction beginsession() {
 		EntityManager em=createEM();
 		return em.getTransaction();
 	}
 
-	
+	/*
+	 * Método que devuelve todos los autores
+	 * @Return lista de todos los autores existentes en la base de datos
+	 * */
 	public static List<Author> getAll() throws DAOException {
 		List<Author> result=new ArrayList<Author>();
 		
 		return result;
 	}
 
+	/*
+	 * Método que borra el autor especificado
+	 * @Param Author que se quiera borrar
+	 * @Return Booleano que devuelve verdadero si se ha borrado el autor y falso y no lo ha conseguido guardar
+	 * */
 	public static boolean delete(Author a) throws DAOException {
 		boolean result=false;
+		EntityManager em=createEM();
 		try {
-			EntityManager em=createEM();
 			em.getTransaction().begin();
 			em.remove(a);
 			em.getTransaction().commit();
@@ -44,34 +62,53 @@ public class AuthorImpH2{
 		} catch (Exception e) {
 			result=false;
 			throw new DAOException("Can´t delete",e);
+		}finally {
+			em.close();
 		}
 		return result;
 	}
 
+	/*
+	 * Método que se usa para guardar un autor
+	 * @Param Author
+	 * @Return booleano que devuelve verdadero si se ha conseguido guardar el autor
+	 * */
 	public static boolean save(Author a) throws DAOException {
 		boolean result=false;
 		EntityManager em=createEM();
 		try {
 			em.getTransaction().begin();
 			em.persist(a);
-			em.getTransaction().begin();
+			em.getTransaction().commit();
 			result=true;
 		} catch (Exception e) {
 			result=false;
 			throw new DAOException("Can´t save",e);
 		}finally {
-			
+			em.close();
 		}
 		
 		return result;
 	}
 
+	/*
+	 * Método que se usa para buscar un autor por id en la base de datos
+	 * @Param id del autor que se quiera buscar
+	 * @Return Autor que tiene la id correspendiente en caso de que se haya encontrado o id=-1 si el autor no se ha encontrado
+	 * 
+	 * */
 	public static Author getByID(long id) throws DAOException {
 		Author result=new Author();
 		
 		return result;
 	}
 
+	/*
+	 * Método que se usa para buscar autores por el nombre
+	 * @Param String con el nombre del autor que se desa buscar
+	 * @Return List<Author> con los autores que coiincidan con dicho nombre, en caso de no encontrar ninguno devuelve una lista vacia
+	 * */
+	
 	public static List<Author> getByName(String name) throws DAOException {
 		List<Author> result=new ArrayList<Author>();
 		
